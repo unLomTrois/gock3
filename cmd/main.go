a555131d42c1b02581f2e8c19de032ea17608fb4
unlomtrois <vadim.kuz02@gmail.com>
2024-09-10 14:25:19 +0300

    refactor: :recycle: split main
    made it easier to read

--- cmd/main.go
+++ cmd/main.go
@@ -1,88 +1,104 @@
 package main
 
 import (
-	// "ck3-parser/internal/app/linter"
-
 	"ck3-parser/internal/app/lexer"
 	"ck3-parser/internal/app/linter"
 	"ck3-parser/internal/app/parser"
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"os"
+	"path/filepath"
 	"time"
 )
 
-func main() {
-	start := time.Now()
+const (
+	inputFilePath = "data/0_elementary.txt"
+	outputDir     = "tmp"
+	parseTreeFile = "parsetree.json"
+	lintedFile    = "linted.txt"
+)
 
-	// Open file
-	filepath := "data/0_elementary.txt"
-	file, err := os.Open(filepath)
+func main() {
+	if err := run(); err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+}
+
+func run() error {
+	start := time.Now()
+	defer func() {
+		log.Printf("Total execution time: %s", time.Since(start))
+	}()
+
+	content, err := readFile(inputFilePath)
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("reading file: %w", err)
+	}
+
+	tokenStream, err := scanContent(content)
+	if err != nil {
+		return fmt.Errorf("scanning content: %w", err)
+	}
+
+	parseTrees, err := parseTokens(tokenStream)
+	if err != nil {
+		return fmt.Errorf("parsing tokens: %w", err)
+	}
+
+	if err := saveJSON(parseTrees, parseTreeFile); err != nil {
+		return fmt.Errorf("saving parse tree: %w", err)
+	}
+
+	if err := lintAndSave(parseTrees); err != nil {
+		return fmt.Errorf("linting and saving: %w", err)
+	}
+
+	return nil
+}
+
+func readFile(path string) ([]byte, error) {
+	start := time.Now()
+	defer func() {
+		log.Printf("File read time: %s", time.Since(start))
+	}()
+
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
 	}
 	defer file.Close()
 
-	filecontent, err := io.ReadAll(file)
-	if err != nil {
-		panic(err)
-	}
-	log.Printf("Read took %s", time.Since((start)))
-	read := time.Now()
-
-	lexer := lexer.New(filecontent)
-	tokenstream, err := lexer.Scan()
-	if err != nil {
-		panic(err)
-	}
-	log.Printf("Scan took %s", time.Since((read)))
-
-	scan := time.Now()
-
-	// err = SaveJSON(tokenstream, "tokenstream.json")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// log.Println("Tokenstream saved to tmp/tokenstream.json")
-
-	parser := parser.New(tokenstream)
-	parsetree := parser.Parse()
-	log.Printf("Parse  took %s", time.Since((scan)))
-
-	if err = SaveJSON(parsetree, "parsetree.json"); err != nil {
-		panic(err)
-	}
-	log.Println("Parsed data saved to tmp/parsetree.json")
-
-	// Lint file
-	lintconfig := linter.LintConfig{
-		IntendStyle:            linter.TABS,
-		IntendSize:             4,
-		TrimTrailingWhitespace: true,
-		InsertFinalNewline:     true,
-		CharSet:                "utf-8-bom",
-		EndOfLine:              []byte("\r\n"),
-	}
-
-	linter := linter.New(parsetree, lintconfig)
-	linter.Lint()
-	if err = linter.Save("tmp/linted.txt"); err != nil {
-		panic(err)
-	}
-	log.Println("Linted file saved to tmp/linted.txt")
-
-	log.Printf("All took %s", time.Since(start))
+	return io.ReadAll(file)
 }
 
-func SaveJSON(data interface{}, filename string) error {
-	err := os.MkdirAll("tmp", 0755)
-	if err != nil {
+func scanContent(content []byte) (*lexer.TokenStream, error) {
+	start := time.Now()
+	defer func() {
+		log.Printf("Scan time: %s", time.Since(start))
+	}()
+
+	l := lexer.New(content)
+	return l.Scan()
+}
+
+func parseTokens(tokens *lexer.TokenStream) ([]*parser.Node, error) {
+	start := time.Now()
+	defer func() {
+		log.Printf("Parse time: %s", time.Since(start))
+	}()
+
+	p := parser.New(tokens)
+	return p.Parse(), nil
+}
+
+func saveJSON(data interface{}, filename string) error {
+	if err := os.MkdirAll(outputDir, 0755); err != nil {
 		return err
 	}
 
-	filepath := "tmp/" + filename
-	file, err := os.Create(filepath)
+	file, err := os.Create(filepath.Join(outputDir, filename))
 	if err != nil {
 		return err
 	}
@@ -95,5 +111,27 @@ func SaveJSON(data interface{}, filename string) error {
 		return err
 	}
 
+	log.Printf("Saved JSON to %s", filepath.Join(outputDir, filename))
+	return nil
+}
+
+func lintAndSave(parseTrees []*parser.Node) error {
+	config := linter.LintConfig{
+		IntendStyle:            linter.TABS,
+		IntendSize:             4,
+		TrimTrailingWhitespace: true,
+		InsertFinalNewline:     true,
+		CharSet:                "utf-8-bom",
+		EndOfLine:              []byte("\r\n"),
+	}
+
+	l := linter.New(parseTrees, config)
+	l.Lint()
+
+	if err := l.Save(filepath.Join(outputDir, lintedFile)); err != nil {
+		return err
+	}
+
+	log.Printf("Linted file saved to %s", filepath.Join(outputDir, lintedFile))
 	return nil
 }
