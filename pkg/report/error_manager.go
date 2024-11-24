78db3d4cdbed9ee7acca8882e5a9a9a2e488fe8e
unlomtrois <unlomtrois@yandex.ru>
2024-11-24 11:11:29 +0300

    log errors with their caller

--- pkg/report/error_manager.go
+++ pkg/report/error_manager.go
@@ -1,10 +1,16 @@
 package report
 
-import "log"
+import (
+	"fmt"
+	"path/filepath"
+	"runtime"
+)
 
 type ErrorManager struct {
 	errors []*DiagnosticItem
 }
+
+// todo: possibly we need to add mutex here
 
 func NewErrorManager() *ErrorManager {
 	return &ErrorManager{
@@ -13,10 +19,24 @@ func NewErrorManager() *ErrorManager {
 }
 
 func (e *ErrorManager) AddError(item *DiagnosticItem) {
-	log.Println(item.Msg, "line:", item.Pointer.Loc.Line)
+	file, line := getCallerInfo(2) // Adjust skip to 2 to get caller of AddError.
+
+	fmt.Printf("%s:%d %s line: %d\n", file, line, item.Msg, item.Pointer.Loc.Line)
+
 	e.errors = append(e.errors, item)
 }
 
 func (e *ErrorManager) Errors() []*DiagnosticItem {
 	return e.errors
+}
+
+// getCallerInfo retrieves the caller's file, line number, and function name.
+func getCallerInfo(skip int) (file string, line int) {
+	_, fullPath, line, ok := runtime.Caller(skip)
+	if !ok {
+		fullPath = "unknown"
+		line = 0
+	}
+	file = filepath.Base(fullPath)
+	return
 }
